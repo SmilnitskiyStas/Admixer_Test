59de6f9de1941bc2d75b2e2988b70e067e960b9b
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2024-03-21 19:21:53 +0200

    Project C# and JS done

---
+++ Admixer_Test/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace Admixer_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Вкажіть колір їжаків де\n0. Red\n1. Green\n2. Blue");
+            int desiredColor = int.Parse(Console.ReadLine());
+
+            int[] population = { 8, 1, 9 };
+
+            int meetings = ChangeColor(population, desiredColor);
+            if (meetings == -1)
+                Console.WriteLine("Неможливо змінити колір всім їжакам");
+            else
+                Console.WriteLine("Мінімальна кількість зустрічей: " + meetings);
+        }
+        static int ChangeColor(int[] population, int desiredColor)
+        {
+            bool allSameColor = true;
+
+            foreach (int count in population)
+            {
+                if (count != 0 && count != population[desiredColor])
+                {
+                    allSameColor = false;
+                    break;
+                }
+            }
+
+            if (allSameColor)
+                return 0;
+
+            int meetings = 0;
+
+            while (true)
+            {
+                bool changed = false;
+                for (int i = 0; i < population.Length; i++)
+                {
+                    for (int j = i + 1; j < population.Length; j++)
+                    {
+                        if (population[i] != 0 && population[j] != 0 && i != j)
+                        {
+                            int thirdColor = 3 - i - j;
+                            int minCount = Math.Min(population[i], population[j]); // Визначаємо кількість зустрічей
+                            population[i] -= minCount;
+                            population[j] -= minCount;
+                            population[thirdColor] += minCount;
+                            meetings += minCount;
+                            changed = true;
+                        }
+                    }
+                }
+
+                if (population[desiredColor] == population[0] + population[1] + population[2])
+                    break;
+
+                if (!changed)
+                    return -1;
+            }
+
+            return meetings;
+        }
+    }
+}
---
+++ Admixer_Test/Sudoku.js
@@ -0,0 +1,55 @@
+﻿function validSolution(board) {
+    // Перевірка рядків
+    for (let row = 0; row < 9; row++) {
+        let rowSet = new Set();
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] === 0 || rowSet.has(board[row][col])) {
+                return false;
+            }
+            rowSet.add(board[row][col]);
+        }
+    }
+
+    // Перевірка стовпчиків
+    for (let col = 0; col < 9; col++) {
+        let colSet = new Set();
+        for (let row = 0; row < 9; row++) {
+            if (board[row][col] === 0 || colSet.has(board[row][col])) {
+                return false;
+            }
+            colSet.add(board[row][col]);
+        }
+    }
+
+    // Якщо всі перевірки пройдені, то дошка є правильним розв'язком
+    return true;
+}
+
+// Приклад використання
+const boardTrue = validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]);
+
+const boardFalse = validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]);
+
+
+console.log(boardTrue); // Повинно вивести true, це правильний розв'язок
+console.log(boardFalse); // Повинно вивести false, це не правильний розв'язок
